f38901e00511f4cc0c4c655147b7a8d8757fb2f7
davphestark <davphestark@gmail.com>
2017-09-17 22:09:04 -0400

    Step 2 & 3: Refactoring
    Maintainability Index 82, Cyclomatic Complexity 30, LOC 48

--- 99BottlesDemo/99BottlesDemo/Verse.cs
+++ 99BottlesDemo/99BottlesDemo/Verse.cs
@@ -13,27 +13,38 @@ namespace _99BottlesDemo
         }
         private string SetLyricFromNumber()
         {
-            string lyric;
-            switch (VerseNumber)
-            {
-                case 2:
-                    lyric = $"{VerseNumber} bottles of beer on the wall, {VerseNumber} bottles of beer. \n" +
-                            $"Take one down and pass it around, {VerseNumber -1} bottle of beer on the wall.";
-                    break;
-                case 1:
-                    lyric = $"{VerseNumber} bottle of beer on the wall, {VerseNumber} bottle of beer. \n" +
-                            $"Take it down and pass it around, no more bottles of beer on the wall.";
-                    break;
-                case 0:
-                    lyric = $"No more bottles of beer on the wall, no more bottles of beer. \n" +
-                            $"Go to the store and buy some more, 99 bottles of beer on the wall.";
-                    break;
-                default:
-                    lyric = $"{VerseNumber} bottles of beer on the wall, {VerseNumber} bottles of beer. \n" +
-                            $"Take one down and pass it around, {VerseNumber -1} bottles of beer on the wall.";
-                    break;
-            }
-            return lyric;       
+            return $"{Capitalize(Quanity(VerseNumber))} {Container(VerseNumber)} of beer on the wall, " +
+                $"{Quanity(VerseNumber)} {Container(VerseNumber)} of beer. \n" +
+                $"{Action(VerseNumber)}, " +
+                $"{Quanity(Next(VerseNumber))} {Container(Next(VerseNumber))} of beer on the wall.";
+        }
+
+        private int Next(int num)
+        {
+            return num == 0 ? 99 : num - 1;
+        }
+
+        private string Quanity(int num)
+        {
+            return num == 0 ? "no more" : num.ToString();
+        }
+
+        private string Pronoun(int num)
+        {
+            return num == 0 ? "it" : "one";
+        }
+
+        private string Container(int num)
+        {
+            return num == 1 ? "bottle" : "bottles";
+        }
+        private string Capitalize(string value)
+        {
+            return value.Substring(0, 1).ToUpper() + value.Substring(1);
+        }
+        private string Action(int num)
+        {
+            return num == 0 ? "Go to the store and buy some more" : $"Take {Pronoun(num - 1)} down and pass it around";
         }
     }
 }
