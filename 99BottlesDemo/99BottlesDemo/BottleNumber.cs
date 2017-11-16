3e40a498cd66d2076c4a73bcb0a930e237ebe65c
davphestark <davphestark@gmail.com>
2017-11-15 23:06:22 -0500

    Step 4 extracting methods into class
    Maintainability Index 84, Cyclomatic Complexity 35, LOC 47

---
+++ 99BottlesDemo/99BottlesDemo/BottleNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace _99BottlesDemo
+{
+    public class BottleNumber
+    {
+        private int number;
+
+        public BottleNumber(int number)
+        {
+            this.number = number;
+        }
+
+        public int Next => number == 0 ? 99 : number - 1;
+
+        public string Quanity => number == 0 ? "no more" : number.ToString();
+
+        public string Pronoun => Next == 0 ? "it" : "one";
+        
+        public string Container => number == 1 ? "bottle" : "bottles";
+       
+        public string Action => number == 0 ? "Go to the store and buy some more" : $"Take {Pronoun} down and pass it around";
+
+        public string QuanityNext => Next == 0 ? "no more" : Next.ToString();
+
+        public string ContainerNext => Next == 1 ? "bottle" : "bottles";
+    }
+}
--- 99BottlesDemo/99BottlesDemo/Verse.cs
+++ 99BottlesDemo/99BottlesDemo/Verse.cs
@@ -6,45 +6,23 @@ namespace _99BottlesDemo
     {
         public int VerseNumber { get; set; }
         public string Lyric { get; set; }
+
         public Verse(int number)
         {
             VerseNumber = number;
-            Lyric = SetLyricFromNumber();
+            Lyric = SetLyricFromNumber(new BottleNumber(number));
         }
-        private string SetLyricFromNumber()
+        private string SetLyricFromNumber(BottleNumber bottleNumber)
         {
-            return $"{Capitalize(Quanity(VerseNumber))} {Container(VerseNumber)} of beer on the wall, " +
-                $"{Quanity(VerseNumber)} {Container(VerseNumber)} of beer. \n" +
-                $"{Action(VerseNumber)}, " +
-                $"{Quanity(Next(VerseNumber))} {Container(Next(VerseNumber))} of beer on the wall.";
+            return $"{Capitalize(bottleNumber.Quanity)} {bottleNumber.Container} of beer on the wall, " +
+                $"{bottleNumber.Quanity} {bottleNumber.Container} of beer. \n" +
+                $"{bottleNumber.Action}, " +
+                $"{bottleNumber.QuanityNext} {bottleNumber.ContainerNext} of beer on the wall.";
         }
-
-        private int Next(int num)
-        {
-            return num == 0 ? 99 : num - 1;
-        }
-
-        private string Quanity(int num)
-        {
-            return num == 0 ? "no more" : num.ToString();
-        }
-
-        private string Pronoun(int num)
-        {
-            return num == 0 ? "it" : "one";
-        }
-
-        private string Container(int num)
-        {
-            return num == 1 ? "bottle" : "bottles";
-        }
+        
         private string Capitalize(string value)
         {
             return value.Substring(0, 1).ToUpper() + value.Substring(1);
-        }
-        private string Action(int num)
-        {
-            return num == 0 ? "Go to the store and buy some more" : $"Take {Pronoun(num - 1)} down and pass it around";
         }
     }
 }
